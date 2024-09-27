91e58c3eff32334c05fd7a4a125a6b900f17f6ca
Milana <milana15.10.05@gmail.com>
2024-09-27 01:10:50 -0400

    Updated files
    In this commit css, js and html files were updated.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Video section
+// musr create an instance using JS to be able to customize
+const player = new Plyr('video');
+
+//burger-con
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+//menu links scroll
+const links = document.querySelectorAll(".nav-item > a");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function (e) {
+        e.preventDefault(); 
+        setTimeout(() => {
+            document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({ behavior: "smooth" });
+        }, 500); 
+    };
+}
